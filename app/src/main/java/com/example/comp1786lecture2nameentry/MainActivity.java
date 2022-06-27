4be31b7c770c7d9512fdedf5bf674277ab75c96e
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-06-27 15:37:23 +0700

    Adding behavior

--- app/src/main/java/com/example/comp1786lecture2nameentry/MainActivity.java
+++ app/src/main/java/com/example/comp1786lecture2nameentry/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.comp1786lecture2nameentry;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText nameText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameText = (EditText) findViewById(R.id.name);
+    }
+
+    public void handleButtonClick(View view){
+        nameText.setText("The button was clicked");
     }
 }
